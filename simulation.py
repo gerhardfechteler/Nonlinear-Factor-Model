8d80e62add27d16818416e80ef251e96022d7044
gerhardfechteler <99479731+gerhardfechteler@users.noreply.github.com>
2022-02-20 21:58:11 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -2,8 +2,12 @@ import numpy as np
 from nDFM_module import nDFM, TimeSeriesMLP, VAR, DFM, nDFM_simulator
 from keras.backend import clear_session
 import matplotlib.pyplot as plt
+import pandas as pd
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
 
 # np.random.seed(2)
+
+
 ###############################################################################
 # Data generation
 
@@ -13,9 +17,8 @@ k = 50
 T_train = 1000
 T_test = 1000
 lags_factor_dynamics = 2
-lags_idiosyncratic_dynamics = 2
+lags_idiosyncratic_dynamics = 1
 p_nonlin = 0.8
-p_sparse = 0.1
 signal_noise_ratio = 2
 
 maxlags = max([lags_factor_dynamics, lags_idiosyncratic_dynamics])
@@ -23,16 +26,136 @@ maxlags = max([lags_factor_dynamics, lags_idiosyncratic_dynamics])
 # Simulate series
 simulator = nDFM_simulator(k, r, T_test + T_train, 
                            lags_factor_dynamics, lags_idiosyncratic_dynamics,
-                           p_nonlin, p_sparse, signal_noise_ratio)
+                           p_nonlin, signal_noise_ratio)
 simulation = simulator.simulate()
 X = simulation['X']
-
-plt.figure(figsize=(10,7))
-plt.plot(simulation['X_no_noise'][100:200,0:10])
 
 # Create train and test data
 X_train = X[:T_train,:]
 X_test = X[T_train:,:]
+
+
+###############################################################################
+# Visualization of propervies of simulated series
+
+# Factor and series correlation table
+F_corr = pd.DataFrame(simulation['factors']).corr()
+X_corr = pd.DataFrame(simulation['X']).corr()
+
+# Plotting parameters update to enable LaTeX
+plt.rcParams.update({
+    "text.usetex": True,
+    # "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"]})
+
+# Plot simulated data series
+plt.figure(figsize=(10,13))
+plt.subplot(3,1,1)
+plt.plot(simulation['factors'][100:200,0:10])
+plt.ylabel('Factors $F_t$')
+plt.subplot(3,1,2)
+plt.plot(simulation['idiosyncratic_noise'][100:200,0:10])
+plt.ylabel('Idiosyncratic noise $\epsilon_t$')
+plt.subplot(3,1,3)
+plt.plot(simulation['X'][100:200,0:10])
+plt.ylabel('Simulated series $X_t$')
+plt.xlabel('Time t')
+plt.suptitle('Simulated factors, idiosyncratic noise and series\n \
+             factor lags = {}, idisyncratic noise lags = {}, \
+             signal-noise ratio = {}, $r={}$, $k={}$ (first 10 plotted), \
+             $p_{{nonlin}}={}$'
+             .format(lags_factor_dynamics, lags_idiosyncratic_dynamics, 
+                     signal_noise_ratio, r, k, p_nonlin))
+plt.tight_layout()
+
+# Plot autocorrelations for factors and series
+plt.figure(figsize=(10,7))
+lags=10
+for i in range(5):
+    plt.subplot(3,5,i+1)
+    ax = plt.gca()
+    plot_pacf(simulation['factors'][:,i], ax=ax, method='ywm', lags=lags)
+    plt.ylim([-0.2,1.1])
+    plt.title('$F_{{{},t}}$'.format(i+1))
+    if i==0:
+        plt.ylabel('Partial autocorrelation')
+for i in range(5):
+    plt.subplot(3,5,5+i+1)
+    ax = plt.gca()
+    plot_pacf(simulation['idiosyncratic_noise'][:,i], ax=ax, method='ywm', lags=lags)
+    plt.ylim([-0.2,1.1])
+    plt.title('$\epsilon_{{{},t}}$'.format(i+1))
+    if i==0:
+        plt.ylabel('Partial autocorrelation')
+for i in range(5):
+    plt.subplot(3,5,10+i+1)
+    ax = plt.gca()
+    plot_pacf(simulation['X'][:,i], ax=ax, method='ywm', lags=lags)
+    plt.ylim([-0.2,1.1])
+    plt.title('$X_{{{},t}}$'.format(i+1))
+    plt.xlabel('Lag')
+    if i==0:
+        plt.ylabel('Partial autocorrelation')
+plt.suptitle('Partial autocorrelation plots for factors, idiosyncratic noise \
+             and series\n factor lags = {}, idisyncratic noise lags = {}, \
+             signal-noise ratio = {}, $r={}$, $k={}$ (first 5 plotted), \
+             $p_{{nonlin}}={}$'
+             .format(lags_factor_dynamics, lags_idiosyncratic_dynamics, 
+                     signal_noise_ratio, r, k, p_nonlin))
+plt.tight_layout()
+
+# Scree plot / eigenvalues
+X_std = (X - np.mean(X,0)) / np.std(X,0)
+XX = X_std.T @ X_std / X.shape[0]
+eig = np.linalg.eig(XX)
+order = np.argsort(eig[0])[::-1]
+eigval = eig[0][order]
+plt.figure(figsize=(10,5))
+plt.scatter(np.arange(1,k+1),eigval, label = "Eigenvalues of $X'X$ in descending order")
+ylim = plt.gca().get_ylim()
+plt.vlines(r, ylim[0], ylim[1], linestyles='--', color='k', label='True number of factors')
+plt.ylim(ylim)
+plt.xlabel('Number of factors')
+plt.ylabel('Eigenvalues')
+plt.title('Scree plot for the simulated series $X$\n \
+          factor lags = {}, idisyncratic noise lags = {}, \
+          signal-noise ratio = {}, $r={}$, $k={}$, \
+          $p_{{nonlin}}={}$'
+          .format(lags_factor_dynamics, lags_idiosyncratic_dynamics, 
+                  signal_noise_ratio, r, k, p_nonlin))
+plt.legend()
+plt.tight_layout()
+
+# Plot transformation functions
+def trafo(x,ind,p=1):
+    if ind==0:
+        return (1-p)*x + p*(np.sign(x)*np.log(1+np.abs(x)))
+    elif ind==1:
+        return (1-p)*x + p*(x-np.sign(x)*np.log(np.abs(x)+1))
+    elif ind==2:
+        return (1-p)*x + p*(x+0.3*np.sign(x)*np.abs(x)**(2))
+    elif ind==3:
+        return (1-p)*x + p*(np.heaviside(1-np.abs(x),0)*x + (1-np.heaviside(1-np.abs(x),0))*np.sign(x))
+    elif ind==4:
+        return (1-p)*x + p*(-1 + x + np.heaviside(x,0)*2)
+    else:
+        raise ValueError('Index for transformation type out of range')
+trafo_text_list = [
+    '$sgn(x)*ln(1+|x|)$',
+    '$x-sgn(x)*ln(1+|x|)$',
+    '$x+0.3*sgn(x)*|x|^2$',
+    '$sgn(x)*min(1,|x|)$',
+    '$sgn(x)*(1+|x|)$']
+x_trafo = np.linspace(-3,3,1000)
+plt.figure(figsize=(10,5))
+plt.plot(x_trafo,x_trafo, '--k', label='linear')
+for i in range(5):
+    plt.plot(x_trafo, trafo(x_trafo, i), label=trafo_text_list[i])
+plt.legend()
+plt.ylabel('Transformation of x')
+plt.xlabel('x')
+plt.title('Transformations used in the function from factors $F$ to series $X$')
+plt.tight_layout()
 
 
 ###############################################################################
